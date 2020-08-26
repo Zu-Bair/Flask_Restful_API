79d8ae473611dcaec5f32caf101e06dc410c146a
Zubair Afzal <zubairc836@gmail.com>
2020-08-26 15:38:00 +0500

    last commit with update functionality

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ video_put_args.add_argument("name", type=str, help="Provide name of video", requ
 video_put_args.add_argument("views", type=int, help="Provide views of video", required=True)
 video_put_args.add_argument("likes", type=int, help="Provide likes on the video", required=True)
 
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Provide name of video", required=True)
+video_update_args.add_argument("views", type=int, help="Provide views of video", required=True)
+video_update_args.add_argument("likes", type=int, help="Provide likes on the video", required=True)
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -38,6 +44,8 @@ class Video(Resource):
         if not result:
             abort(404, message="Could not find video with that id...")
         return result
+
+
     @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
@@ -48,6 +56,22 @@ class Video(Resource):
         db.session.add(video)
         db.session.commit()
         return video, 201
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Cannot Update Video Doesn't Exist...")
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit()
+        return result
     
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
